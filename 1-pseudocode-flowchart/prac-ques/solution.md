40af69559142f6536904c1c70bdfd286aeaea1b8
Developer-Ronnie <ronnieworkspace@gmail.com>
2024-09-06 16:48:29 +0530

    Initial Commit

---
+++ 1-pseudocode-flowchart/prac-ques/solution.md
@@ -0,0 +1,6 @@
+# Solution 1 : 
+
+1. Input a & b 
+2. Sum = a+b 
+3. Print Sum 
+4. Exit
---
+++ 2-Variables-DataTypes-Operators/code.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int age = 234;
+    cout << sizeof(age) << endl;
+    return 0;
+}
