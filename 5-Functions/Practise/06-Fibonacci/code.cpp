1d4a9793778e366b96f94ffde1a2d76a0c389905
Developer-Ronnie <ronnieworkspace@gmail.com>
2024-11-01 17:30:39 +0530

    added fibonacci numbers

--- 5-Functions/Practise/06-Fibonacci/code.cpp
+++ 5-Functions/Practise/06-Fibonacci/code.cpp
@@ -1,5 +1,25 @@
+#include <iostream>
+using namespace std;
 
 
+// Write a function to print the Fibonacci series up to n terms
 
+void printFibonacci(int n) {
+    int t1 = 0, t2 = 1;
+    cout << "Fibonacci series up to " << n << " terms: ";
+    for (int i = 1; i <= n; i++) {
+        cout << t1 << " ";
+        int nextTerm = t1 + t2;
+        t1 = t2;
+        t2 = nextTerm;
+    }
+    cout << endl;
+}
 
-// Write a program to print the Fibonacci series up to n terms
+int main() {
+    int n;
+    cout << "Enter the number of terms: ";
+    cin >> n;
+    printFibonacci(n);
+    return 0;
+}
